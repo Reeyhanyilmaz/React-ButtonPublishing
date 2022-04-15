fbcfe63984d9741a80794f25097e7d4f6001c006
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-04-15 10:57:44 +0300

    chemistdev minor version upgrade

--- test-proje/src/App.js
+++ test-proje/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import "chemistdev/dist/index.css";
-import { Button , ExampleComponent } from "chemistdev";
+// import { Button , ExampleComponent } from "chemistdev";
+import { Button , Paragraph } from "chemistdev";
+
 
 function App() {
   return (
     <div className="App">
      < Button text="Reyhan" />
-     < ExampleComponent text="ben example"/> 
+     {/* < ExampleComponent text="ben example"/>  */}
+     <Paragraph text="Paragraf!" />
     </div>
   );
 }
