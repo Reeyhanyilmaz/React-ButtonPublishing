475110d1974e5ad43444f363573c72dd14e67e32
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-04-15 10:38:29 +0300

    chemistdev patch version upgrade

--- test-proje/src/App.js
+++ test-proje/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import "chemistdev/dist/index.css";
+import { Button , ExampleComponent } from "chemistdev";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     < Button text="Reyhan" />
+     < ExampleComponent text="ben example"/> 
     </div>
   );
 }
